a1fb202b9166590592a3b629fe1545c57dd2bdb7
dranel-AIe <lenarddumalagan25@gmaile.com>
2021-10-23 13:58:17 +0800

    make it better

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const server = require('./server')
 const port = process.env.PORT || '5000'
 
-require('dotenv').config()
-
 server.listen(port, () => {
     const url = `http://localhost:${port}`
     console.table({ url })
